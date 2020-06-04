b212ea100746d654eb3fc1ec218b1661b44797d1
Jason Kim <edujason.hphk@gmail.com>
2020-06-04 20:14:52 +0900

    Filter todos with params

--- todos/models.py
+++ todos/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Todo(models.Model):
     content = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
+    is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
